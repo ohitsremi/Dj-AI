8f7144b65cb5b9a4852087f1dfa9c105dcfac111
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-11-03 09:14:59 -0800

    Initial commit

---
+++ src/bot.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('The bot has logged in.')
+})
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
