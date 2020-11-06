bf35fd14f64a32ff7bf0ddfeb2f394a55018b43e
Remi Srijunyanont <psrijuny@ucsc.edu>
2020-11-06 09:00:17 -0800

    Polling Setup

--- src/bot.js
+++ src/bot.js
@@ -3,10 +3,34 @@ require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = "$";
 
 client.on('ready', () => {
     console.log('The bot has logged in.')
-})
+});
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    if(message.content === '!Dj'){
+        message.channel.send('Song').then(sentMessage =>{
+            sentMessage.react('👍')
+            sentMessage.react('👎');
+        });
+    }
+});
+
+//Commands
+client.on('message', message =>{
+    if(message.author.bot) return;
+    if(message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        console.log(CMD_NAME);
+        console.log(args);
+    }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
