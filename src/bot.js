63b6e1c74c60c2b7134583d012e46a1cca3e08a3
NaNa Mathis <nanarhmathis@gmail.com>
2020-11-09 13:56:23 -0500

    spotify api working, trying to use it in discord bot commands

--- src/bot.js
+++ src/bot.js
@@ -1,33 +1,42 @@
 require('dotenv').config();
+const fs = require('fs');
+const { Discord, Client, Collection } = require('discord.js');
 
-const { Client } = require('discord.js');
 const client = new Client();
-const PREFIX = "$";
+client.commands = new Collection();
+
+const PREFIX = "!";
+const commandFiles = fs.readdirSync(__dirname + '/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
     console.log('The bot has logged in.')
 });
 
 client.on('message', message => {
-    if(message.author.bot) return;
-    if(message.content === '!dj'){
-        message.channel.send('Song').then(sentMessage =>{
-            sentMessage.react('👍')
-            sentMessage.react('👎');
-        });
+    const { author, content } = message;
+    if(author.bot || !content.startsWith(PREFIX)) {
+        return;
     }
-});
-
-//Commands
-client.on('message', message =>{
-    if(message.author.bot) return;
-    if(message.content.startsWith(PREFIX)){
-        const [CMD_NAME, ...args] = message.content
-            .trim()
-            .substring(PREFIX.length)
-            .split(/\s+/);
-        console.log(CMD_NAME);
-        console.log(args);
+    else {
+        const username = `${author.username}#${author.discriminator}`;
+        const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+        if (!client.commands.has(command)) {
+            return;
+        }
+        try {
+            client.commands.get(command).execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
     }
 });
 
--- src/spotify.js
+++ src/spotify.js
@@ -1,9 +1,4 @@
 require('dotenv').config({ path: '../.env' })
-// var express = require('express'); // Express web server framework
-// var request = require('request'); // "Request" library
-// var cors = require('cors');
-// var querystring = require('querystring');
-// var cookieParser = require('cookie-parser');
 
 var SpotifyWebApi = require('spotify-web-api-node');
 const scopes = ['user-read-private', 'user-read-email']
@@ -12,7 +7,6 @@ var client_secret = process.env.SPOTIFY_SECRET; // Your secret
 var redirect_uri = process.env.SPOTIFY_REDIRECT; // Your redirect uri
 
 const express = require('express');
-const { request } = require('express');
 
 const app = express();
 
@@ -55,9 +49,9 @@ app.get('/callback', (req, res) => {
             `Sucessfully retreived access token. Expires in ${expires_in} s.`
         )
 
-        // GENERATE RANDOM SONG SEARCH BASED ON COUNTRY
-        request_valid_song(code, 'country');
-        
+        // TESTING GETTING SONGS
+        getSongsByGenre('pop');
+
         res.send('Success! You can now close the window.');
 
         setInterval(async () => {
@@ -75,74 +69,20 @@ app.get('/callback', (req, res) => {
         });
 });
 
-function getRandomSearch() {
-    // A list of all characters that can be chosen.
-    const characters = 'abcdefghijklmnopqrstuvwxyz';
-    
-    // Gets a random character from the characters string.
-    const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
-    let randomSearch = '';
-  
-    // Places the wildcard character at the beginning, or both beginning and end, randomly.
-    switch (Math.round(Math.random())) {
-      case 0:
-        randomSearch = randomCharacter + '%';
-        break;
-      case 1:
-        randomSearch = '%' + randomCharacter + '%';
-        break;
-    }
-  
-    return randomSearch;
-  }
-
-const randomOffset = Math.floor(Math.random() * 10000);
-
-function request_valid_song(code, genre) {
-    console.log(code);
-    spotifyApi
-        .authorizationCodeGrant(code)
-        .then(function(data) {
-            console.log('data');
-            console.log('Retrieved access token', data.body['access_token']);
-
-            // Set the access token
-            spotifyApi.setAccessToken(data.body['access_token']);
-
-            // Use the access token to retrieve information about the user connected to it
-            genre.replace(' ', '%20');
-            var query = `q=${getRandomSearch()}%20genre:%22${genre}`;
-            var types = ['track'];
-            var offset = randomOffset;
-            return spotifyApi.search(query, types, {offset: offset});
-        })
-        .then(function(data) {
-            // console.log('I got ' + data.body.tracks.total + ' results!');
-            // console.log(data.body.tracks.items);
-            // var artist = song_info['artists'][0]['name']
-            // var song = song_info['name']
-            // if (songs === undefined) {
-            //     artist = "Rick Astley"
-            //     song = "Never Gonna Give You Up"
-            // }
-            // }
-            // Print some information about the results
-            console.log('I got ' + data.body.tracks.total + ' results!');
-
-            // Go through the first page of results
-            var firstPage = data.body.tracks.items;
-            console.log('The tracks in the first page are (popularity in parentheses):');
-            firstPage.forEach(function(track, index) {
-            console.log(index + ': ' + track.name + ' (' + track.popularity + ')');
-            });
-        }).catch(function(err) {
-            console.log('Something went wrong:', err.message);
+function getSongsByGenre(genre) {
+    spotifyApi.getAvailableGenreSeeds();
+    spotifyApi.getRecommendations({limit: 5, seed_genres: [genre]})
+    .then(function(data) {
+        var tracks = data.body.tracks;
+        Object.keys(tracks).forEach((key) => {
+            var trackName = tracks[key]['name'];
+            var isExplicit = tracks[key]['explicit']
+            var trackURI = tracks[key]['uri'];
+            console.log(trackName);
         });
 
+    });
 }
-
-
-// request_valid_song('country');
 
 app.listen(8888, () =>
   console.log(
